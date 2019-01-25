24e9ce0ada85dd530b8d18d812417abc58dd2099
Filip Van der Schueren <filip.vanderschueren@uantwerpen.be>
2019-01-25 20:35:35 +0100

    Class Value added + used in main (main was already committed)

---
+++ src/TestGitHub01/Value.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TestGitHub01;
+
+/**
+ *
+ * @author FVanderSchueren
+ */
+public class Value {
+    private int value;
+
+    //defaut constructor
+    
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+    
+    
+}
